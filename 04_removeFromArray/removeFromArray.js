ff1e03e465ae8c126bc656004db8ab53eae01bc8
Watson Carawon <carawonw@gmail.com>
2024-12-29 18:55:44 +0000

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
 const removeFromArray = function() {
+    const argsArray = Array.from(arguments);
+    const array = argsArray[0];
+
+    let filteredArray = [];
+    for (i = 0; i < array.length; i++) {
+        if (argsArray.slice(1,).includes(array[i])) {
+        }
+        else {
+            filteredArray.push(array[i]);
+        }
+    }
+
+    return filteredArray;
 };
 
 // Do not edit below this line
