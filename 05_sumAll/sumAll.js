ce826af6a1b4111a878be81a7b22c960686ffa3a
Watson Carawon <carawonw@gmail.com>
2024-12-29 19:08:07 +0000

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    if (typeof start !== "number" || typeof end !== "number") {
+        return "ERROR";
+    }
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    let min = (start < end) ? start : end;
+    let max = (start > end) ? start : end;
+
+    for (i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
