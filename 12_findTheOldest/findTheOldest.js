c229e16f363fda62a20442b727d35395b563f451
Watson Carawon <carawonw@gmail.com>
2025-01-08 21:53:50 +0000

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const ages = arr.map((person) => {
+        if (!person.yearOfDeath) {
+            const deathYear = new Date().getFullYear();
+            return deathYear - person.yearOfBirth;
+        }
+        else {
+            return person.yearOfDeath - person.yearOfBirth;
+        }
+    });
 
+    let oldest = 0;
+    ages.forEach(age => {
+        if (age > oldest) {
+            oldest = age;
+        }
+    });
+    return arr[ages.indexOf(oldest)];
 };
 
 // Do not edit below this line
