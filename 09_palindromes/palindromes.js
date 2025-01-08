f4f06a9c93ec7465270fab258f5025c32518b8cf
Watson Carawon <carawonw@gmail.com>
2025-01-08 20:57:12 +0000

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanString = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    const reverseCleanString = cleanString.split("").reverse().join("");
+    return cleanString === reverseCleanString;
 };
 
 // Do not edit below this line
