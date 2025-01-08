4023426476a1af5717d593aad2cfb7e1efc5eb8b
Watson Carawon <carawonw@gmail.com>
2025-01-08 21:22:03 +0000

    Complete fibonnaci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(N) {
+    let num = parseInt(N);
+    if (num < 0) {
+        return "OOPS";
+    }
+    if (num === 0) {
+        return 0;
+    }
+    else if (num === 1) {
+        return 1;
+    }
 
+    let arr = [1, 1];
+    num -= 2;
+    while (num !== 0) {
+        arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+        num--;
+    }
+    return arr[arr.length - 1];
 };
 
 // Do not edit below this line
