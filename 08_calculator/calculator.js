be763313ed77a9a627d30f117f01fd832d57c070
Watson Carawon <carawonw@gmail.com>
2025-01-08 20:22:43 +0000

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+//@ts-check
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, curr) => {
+    return total + curr;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((total, curr) => {
+    return total * curr;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  if (x === 0) {
+    return 1;
+  }
+  else {
+    return x * factorial(x - 1);
+  }
 };
 
 // Do not edit below this line
